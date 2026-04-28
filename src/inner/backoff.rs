380ad7b5a89e1386a03fdf6c51fd93ad38d7e922
Aaron-json <90925260+Aaron-json@users.noreply.github.com>
2026-04-27 20:18:09 -0600

    refine backoff implementation

--- src/inner/backoff.rs
+++ src/inner/backoff.rs
@@ -1,34 +1,30 @@
 // Maximum spin value
 const SPIN_MAX: u32 = 6;
 // Maximum stems
-const PAUSE_MAX: u32 = 8;
+const YIELD_MAX: u32 = 8;
 
 pub struct Backoff {
     count: u32,
 }
 
 impl Backoff {
-    #[inline]
+    #[inline(always)]
     pub fn new() -> Self {
         Self { count: 0 }
     }
 
-    // Implements a spin loop with exponential backoff.
-    // Does not yield/park
-    //
-    // Useful when retrying a failed operation.
+    // Implements backoff with pure spinning.
     #[inline]
     pub fn spin(&mut self) {
         for _ in 0..(1 << self.count.min(SPIN_MAX)) {
             std::hint::spin_loop();
         }
 
-        if self.count <= SPIN_MAX {
-            self.count += 1;
-        }
+        self.count = self.count.saturating_add(1);
     }
 
-    // Implements backoff with potential blocking.
+    // Implements backoff by spinning first, then yielding.
+    #[inline]
     pub fn pause(&mut self) {
         if self.count <= SPIN_MAX {
             for _ in 0..(1 << self.count) {
@@ -41,10 +37,16 @@ impl Backoff {
         self.count = self.count.saturating_add(1);
     }
 
-    // Returns if Backoff has maximized its counter limits
-    // and the user should probably implement blocking or other
-    // more appropriate solutions.
-    pub fn saturated(&mut self) -> bool {
-        return self.count >= PAUSE_MAX;
+    // Returns whether we should give up backoff attempts and resort to
+    // another strategy.
+    #[inline(always)]
+    pub fn done(&mut self) -> bool {
+        return self.count >= YIELD_MAX;
+    }
+
+    // Resets the backoff for reuse.
+    #[inline(always)]
+    pub fn reset(&mut self) {
+        self.count = 0;
     }
 }
