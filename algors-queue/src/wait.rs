4f914239807b47c464cd464f6ae7124ee5faa6e9
Aaron-json <90925260+Aaron-json@users.noreply.github.com>
2026-04-30 17:13:36 -0600

    implement aborting/error for wait strategies

--- algors-queue/src/wait.rs
+++ algors-queue/src/wait.rs
@@ -5,73 +5,88 @@ extern crate alloc;
 use std::sync::{Condvar, Mutex};
 
 pub trait WaitStrategy {
-    fn wait_for<F, T>(&self, op: F) -> T
+    // Error type is left up to implementation.
+    //
+    // We avoid a generic error since one strategy could have multiple
+    // implementations with different error types, which adds complexity.
+    //
+    // If a single implementation could have different approaches, then they
+    // should be split into different implementations.
+    type Error;
+
+    // Wait for accepts an op function that is waited for, for successful
+    // completion. Some strategies may support aborting/giving up
+    // (e.x timeout).
+    fn wait_for<F, T>(&self, op: F) -> Result<T, Self::Error>
     where
         F: FnMut() -> Option<T>;
     fn notify(&self);
 }
 
-// Implements a spin-only wait strategy.
+/// Implements a spin-only wait strategy.
+#[derive(Default)]
 pub struct SpinWait;
 
 impl WaitStrategy for SpinWait {
-    fn wait_for<F, T>(&self, mut op: F) -> T
+    type Error = core::convert::Infallible;
+
+    fn wait_for<F, T>(&self, mut op: F) -> Result<T, Self::Error>
     where
         F: FnMut() -> Option<T>,
     {
         let mut backoff = Backoff::new();
         loop {
             if let Some(val) = op() {
-                return val;
+                return Ok(val);
             };
             backoff.spin();
         }
     }
+
     fn notify(&self) {}
 }
 
-// Implements a wait strategy that starts with a spin wait and then
-// attempts to yield to the OS when the spin limit is reached.
-//
-// This strategy holds shared memory and thus users must share the
-// same object.
+/// Implements a wait strategy that starts with a spin wait and then
+/// attempts to yield to the OS when the spin limit is reached.
+#[derive(Default)]
 pub struct YieldWait;
 
 impl WaitStrategy for YieldWait {
-    fn wait_for<F, T>(&self, mut op: F) -> T
+    type Error = core::convert::Infallible;
+
+    fn wait_for<F, T>(&self, mut op: F) -> Result<T, Self::Error>
     where
         F: FnMut() -> Option<T>,
     {
         let mut backoff = Backoff::new();
         loop {
             if let Some(val) = op() {
-                return val;
+                return Ok(val);
             };
             backoff.pause();
         }
     }
+
     fn notify(&self) {}
 }
 
+/// Implements a blocking wait strategy. This is only available with
+/// the `std` feature compiled since it relies on OS features.
+///
+/// This strategy holds shared memory and thus users must share the
+/// same object.
 #[cfg(feature = "std")]
+#[derive(Default)]
 pub struct BlockingWait {
     state: Mutex<u64>,
     cvar: Condvar,
 }
 
 #[cfg(feature = "std")]
-impl BlockingWait {
-    pub fn new() -> Self {
-        Self {
-            state: Mutex::new(0),
-            cvar: Condvar::new(),
-        }
-    }
-}
-
-#[cfg(feature = "std")]
 impl WaitStrategy for BlockingWait {
-    fn wait_for<F, T>(&self, mut op: F) -> T
+    type Error = core::convert::Infallible;
+
+    fn wait_for<F, T>(&self, mut op: F) -> Result<T, Self::Error>
     where
         F: FnMut() -> Option<T>,
     {
@@ -79,14 +94,14 @@ impl WaitStrategy for BlockingWait {
         loop {
             if !backoff.done() {
                 if let Some(val) = op() {
-                    return val;
+                    return Ok(val);
                 }
                 backoff.spin();
             } else {
                 let prev = *self.state.lock().unwrap();
 
                 if let Some(val) = op() {
-                    return val;
+                    return Ok(val);
                 }
 
                 let mut cur = self.state.lock().unwrap();
@@ -100,6 +115,7 @@ impl WaitStrategy for BlockingWait {
             }
         }
     }
+
     fn notify(&self) {
         let mut state = self.state.lock().unwrap();
         *state += 1;
